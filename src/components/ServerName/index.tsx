a79262438bbee6241deb4845b669d10eae0ce582
Fernando Borges <fernandoborgesjr@outlook.com>
2020-12-31 19:37:13 -0300

    Server Name component created

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title, ExpandIcon } from "./styles";
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Servidor Brabo</Title>
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ServerName;
