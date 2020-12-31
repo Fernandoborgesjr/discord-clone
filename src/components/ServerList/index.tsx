f94a741c3e169a9dad20faaebac7ce36256191e2
Fernando Borges <fernandoborgesjr@outlook.com>
2020-12-31 17:48:59 -0300

    Server List created

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Container, Separator } from "./styles";
+import ServerButton from "../ServerButton";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+      <Separator />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={4} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
